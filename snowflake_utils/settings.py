819a4c8316cd65596b270e32728cacc2e5f67df4
Paolo Quadri <paolo.quadri@remote.com>
2024-09-19 17:29:38 +0200

    feat: allow multiple authentication methods

--- snowflake_utils/settings.py
+++ snowflake_utils/settings.py
@@ -1,29 +1,52 @@
+from enum import Enum
+from typing import Annotated
+
+from pydantic import StringConstraints
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from snowflake.connector import SnowflakeConnection
 from snowflake.connector import connect as _connect
+
+
+class Authenticator(str, Enum):
+    snowflake = "snowflake"
+    externalbrowser = "externalbrowser"
+    username_password_mfa = "username_password_mfa"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+OktaDomain = Annotated[
+    str,
+    StringConstraints(pattern=r"https://.*\.okta\.com"),
+]
 
 
 class SnowflakeSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="SNOWFLAKE_")
 
     account: str = "snowflake-test"
-    user: str = "snowlfake"
-    password: str = "snowlfake"
-    db: str = "snowlfake"
-    role: str = "snowlfake"
-    warehouse: str = "snowlfake"
+    user: str = "snowflake"
+    password: str = "snowflake"
+    db: str = "snowflake"
+    role: str = "snowflake"
+    warehouse: str = "snowflake"
+    authenticator: Authenticator | OktaDomain = Authenticator.snowflake
     _schema: str | None = None
 
     def creds(self) -> dict[str, str | None]:
-        return {
+        base_creds = {
             "account": self.account,
             "user": self.user,
-            "password": self.password,
             "database": self.db,
             "schema": self._schema,
             "role": self.role,
             "warehouse": self.warehouse,
+            "authenticator": str(self.authenticator),
         }
+        if self.authenticator in (Authenticator.externalbrowser):
+            return base_creds
+        return base_creds | {"password": self.password}
 
     def connect(self) -> SnowflakeConnection:
         return _connect(**self.creds())
