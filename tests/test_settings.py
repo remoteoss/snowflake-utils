3aa58a9e493804fc2874284107868e87a6e78ce7
Paolo Quadri <pquadri10@gmail.com>
2025-03-25 14:24:54 +0100

    feat: handle already full qualified tags (#17)

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,7 +1,7 @@
 import pytest
 from pydantic import ValidationError
 
-from snowflake_utils.settings import SnowflakeSettings
+from snowflake_utils.settings import GovernanceSettings, SnowflakeSettings
 
 
 @pytest.mark.parametrize(
@@ -21,3 +21,15 @@ def test_authenticator(authenticator: str) -> None:
 def test_authenticator_invalid() -> None:
     with pytest.raises(ValidationError):
         SnowflakeSettings(authenticator="invalid")
+
+
+@pytest.mark.parametrize(
+    "object_name",
+    [
+        "table",
+        "governance.public.table",
+    ],
+)
+def test_governance_settings(object_name: str) -> None:
+    settings = GovernanceSettings()
+    assert settings.fqn(object_name) == "governance.public.table"
